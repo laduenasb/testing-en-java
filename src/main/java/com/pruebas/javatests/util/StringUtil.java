5977ff15c6e5c4af27bd48c0306c98102c37cfa4
Alejandro Dueñas <luisdue3@gmail.com>
2024-04-12 11:59:02 -0500

    Test unitario al metodo repeat de la clase StringUtil sin librerias - clase 4

--- src/main/java/com/pruebas/javatests/util/StringUtil.java
+++ src/main/java/com/pruebas/javatests/util/StringUtil.java
@@ -3,7 +3,7 @@ package com.pruebas.javatests.util;
 class StringUtil {
   public static String repeat(String str, int times){
     String result = "";
-    for (int ii = 0; ii <= times; ii++) {
+    for (int ii = 0; ii < times; ii++) {
       result += str;
     }
     return result;
