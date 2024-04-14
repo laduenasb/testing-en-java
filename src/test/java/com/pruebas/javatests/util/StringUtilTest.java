8e846c1b198a525653994fe082bdf59cf468c38a
Alejandro Dueñas <luisdue3@gmail.com>
2024-04-14 12:15:32 -0500

    Primer test usando junit 5 (clase 5)

--- src/test/java/com/pruebas/javatests/util/StringUtilTest.java
+++ src/test/java/com/pruebas/javatests/util/StringUtilTest.java
@@ -1,17 +1,14 @@
 package com.pruebas.javatests.util;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringUtilTest {
 
-  public static void main(String[] args) {
-
-    assertEquals(StringUtil.repeat("Hola", 3),"HolaHolaHola");
-    assertEquals(StringUtil.repeat("Hola", 1), "Hola");
-
-  }
-
-  private static void assertEquals(String actual, String expected) {
-    if(!actual.equals(expected)) {
-      throw new RuntimeException(actual + " is not equal to expected " + expected);
-    }
+  @Test
+  public void testRepeat() {
+    assertEquals("HolaHolaHola", StringUtil.repeat("Hola", 3));
+    assertEquals("Hola", StringUtil.repeat("Hola", 1));
   }
 }
