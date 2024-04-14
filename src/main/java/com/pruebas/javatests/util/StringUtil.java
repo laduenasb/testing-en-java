c20f66cb2029b6254f1d333259bc949a922e42d1
Alejandro Dueñas <luisdue3@gmail.com>
2024-04-14 12:44:06 -0500

    Se escribe una función de est por ccada prueba, el nombre del test nos indicara que estamos probando

--- src/main/java/com/pruebas/javatests/util/StringUtil.java
+++ src/main/java/com/pruebas/javatests/util/StringUtil.java
@@ -2,6 +2,9 @@ package com.pruebas.javatests.util;
 
 class StringUtil {
   public static String repeat(String str, int times){
+    if(times<0){
+      throw new IllegalArgumentException("Negative times not allowed");
+    }
     String result = "";
     for (int ii = 0; ii < times; ii++) {
       result += str;
--- src/test/java/com/pruebas/javatests/util/StringUtilTest.java
+++ src/test/java/com/pruebas/javatests/util/StringUtilTest.java
@@ -3,12 +3,24 @@ package com.pruebas.javatests.util;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class StringUtilTest {
 
   @Test
-  public void testRepeat() {
-    assertEquals("HolaHolaHola", StringUtil.repeat("Hola", 3));
+  public void repeat_string_once() {
     assertEquals("Hola", StringUtil.repeat("Hola", 1));
+  }
+  @Test
+  public void repeat_string_multiple_times() {
+    assertEquals("HolaHolaHola", StringUtil.repeat("Hola", 3));
+  }
+  @Test
+  public void repeat_string_zero_times() {
+    assertEquals("", StringUtil.repeat("Hola", 0));
+  }
+  @Test
+  public void repeat_string_negative_times() {
+    assertThrows(IllegalArgumentException.class, () -> StringUtil.repeat("Hola", -1), "Se esperaba una excepción de argumento ilegal");
   }
 }
